c151cdb29edb45b42a02de7c39dd1f0388404606
deep-amol2209 <anmol222006@gmail.com>
2025-03-15 22:36:45 +0530

    change var names in config.js

--- server/config.js
+++ server/config.js
@@ -1,19 +1,15 @@
-import dotenv from "dotenv"
-
+import dotenv from "dotenv";
 dotenv.config();
 
-export const configure={
-
-mongodburi: process.env.mongodburi,
-PORT: process.env.PORT,
-jwtsecret: process.env.jwtsecret,
-admin_password: process.env.admin_password,
-refreshsecret: process.env.refreshsecret,
-cloudinary:{
-    CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME,
-    API_KEY: process.env.CLOUDINARY_API_KEY,
-    API_SECRET: process.env.CLOUDINARY_API_SECRET
-}
-
-
+export const configure = {
+    mongodburi: process.env.MONGODB_URI,  // Fixed variable name
+    PORT: process.env.PORT,
+    jwtsecret: process.env.JWT_SECRET, // Fixed uppercase naming
+    admin_password: process.env.ADMIN_PASSWORD, // Fixed variable name
+    refreshsecret: process.env.REFRESH_SECRET,
+    cloudinary: {
+        CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME,
+        API_KEY: process.env.CLOUDINARY_API_KEY,
+        API_SECRET: process.env.CLOUDINARY_API_SECRET
+    }
 };
