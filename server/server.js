91435c65fcfac0d74171892f1ed770ad04d60afc
deep-amol2209 <anmol222006@gmail.com>
2025-05-07 00:01:54 +0530

    add process.env

--- server/server.js
+++ server/server.js
@@ -22,7 +22,7 @@ import designationRoutes from "./routes/designationRoutes.js"
 
 dotenv.config()
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 
 // Middleware
